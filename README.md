7b940bbe2eaa63ee1badf3d9aaf8b5329e854c8a
Jacopo Farina <jacopo.farina@flixbus.com>
2021-01-15 17:18:47 +0100

    Add phaser dependency and Readme

---
+++ README.md
@@ -0,0 +1,4 @@
+## TODO write proper description here
+
+## Run locally
+Use `yarn install` and then `yarn run dev-serve` to run a local server that will compile and reload on change.
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
+// not used yet really
+import 'phaser';
+
+
 const button = document.getElementById("test-button");
 button.addEventListener("click", (event) => {
-  window.alert('Yep, you clicked')
+  window.alert('Yep, you clicked');
+  return false;
 });
