13eaab29c4d7395f5dd1778d5d0b828051ddf3ca
Jacopo Farina <jacopo.farina@flixbus.com>
2021-01-17 12:30:57 +0100

    Add camera and static sprite example

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 ## Run locally
 Use `yarn install` and then `yarn run dev-serve` to run a local server that will compile and reload on change.
+
+# Credits and assets
+The assets are from Liberated Pixel Cup, see:
+
+* https://opengameart.org/content/2d-lost-garden-tileset-transition-to-jetrels-wood-tileset
+* https://opengameart.org/content/rpg-character
--- src/scenes/main-scene.ts
+++ src/scenes/main-scene.ts
@@ -1,5 +1,7 @@
 export class MainScene extends Phaser.Scene {
   private player: Phaser.Physics.Arcade.Sprite;
+  private demo_world: Phaser.Physics.Arcade.Sprite;
+
   private cursors: Phaser.Types.Input.Keyboard.CursorKeys;
 
   constructor() {
@@ -8,11 +10,16 @@ export class MainScene extends Phaser.Scene {
 
   preload(): void {
     this.load.spritesheet('player', 'assets/sprites/MainGuySpriteSheet.png', { frameWidth: 32, frameHeight: 32 });
+    this.load.spritesheet('world_example', 'assets/sprites/wood_tileset.png', { frameWidth: 32, frameHeight: 32 });
   }
 
   create(): void {
     this.player = this.physics.add.sprite(400, 300, 'player');
     this.cursors = this.input.keyboard.createCursorKeys();
+    this.cameras.main.startFollow(this.player, false, 0.1, 0.1);
+
+    this.demo_world = this.physics.add.sprite(300, 200, 'world_example');
+
   }
 
   update(): void {
