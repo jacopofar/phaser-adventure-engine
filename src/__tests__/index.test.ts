104383512df1282d322e4f01afc2c6a3c95f603a
Jacopo Farina <jacopo.farina@flixbus.com>
2021-01-18 12:38:48 +0100

    Switch to a meaningful test description

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,6 +1,5 @@
 import { GameConfig } from '../config';
 
-test('adds 2 + 2 to equal 4, Winston', () => {
-  expect(2 + 2).toBe(4);
+test('GameConfig is created with the properties from the object', () => {
   expect(GameConfig({gameHeight: 600, gameWidth: 800})).toHaveProperty("height", 600);
 });
