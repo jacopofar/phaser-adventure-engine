7faff8afb7460f32560b809057967c03ac6abc85
Jacopo Farina <jacopo.farina@flixbus.com>
2021-01-20 18:19:17 +0100

    Load a chunk for testing

--- src/scenes/main-scene.ts
+++ src/scenes/main-scene.ts
@@ -21,7 +21,7 @@ export class MainScene extends Phaser.Scene {
     this.player.setDepth(1);
     this.cursors = this.input.keyboard.createCursorKeys();
     this.cameras.main.startFollow(this.player);
-    const chunk = new Chunk(this, 'game/maps/first/just_a_chunk.json', 0, 0);
+    const chunk = new Chunk(this, 'game/maps/second/chunk_0_0.json', 0, 0);
 
   }
 
