79ce35eeedc241564a31805334278e09ee1f5703
Jacopo Farina <jacopo.farina@flixbus.com>
2021-01-28 14:17:42 +0100

    Enable tileset testing with an awful trick

--- src/__tests__/tilesets.test.ts
+++ src/__tests__/tilesets.test.ts
@@ -10,35 +10,32 @@ describe('Tileset retrieval', () => {
   afterEach(() => moxios.uninstall());
 
   test('retrieves a tileset', async () => {
-    const game = new Phaser.Game({
-      disableContextMenu: true,
-      type: Phaser.HEADLESS,
-      width: 800,
-      height: 600,
-      scene: [{
-        preload: () => { console.log('server preload') },
-        create: () => { console.log('server create') },
-        update: () => { console.log('server update') }
-      }]
-    });
-    const loader = new Phaser.Loader.LoaderPlugin(game.scene.getAt(0));
+    const loader = {
+      on: jest.fn<Phaser.Loader.LoaderPlugin, [string, Function]>((evtype, f) => {
+        if (evtype == 'complete') {
+          setImmediate(() => f());
+        }
+        return null as Phaser.Loader.LoaderPlugin;
+      }),
+      spritesheet: jest.fn<Phaser.Loader.LoaderPlugin, [any]>((i) => {
+        return null as Phaser.Loader.LoaderPlugin;
+      }),
+      start: jest.fn<Phaser.Loader.LoaderPlugin, [any]>((i) => {
+        return null as Phaser.Loader.LoaderPlugin;
+      })
+    };
 
-    jest.spyOn(loader, 'on')
-    .mockImplementation(() => loader);
-
-    try {
-      moxios.wait(() => {
-        let request = moxios.requests.mostRecent();
-        request.respondWith({
-          status: 200,
-          response: {e : 42},
-        });
+    moxios.wait(() => {
+      let request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: {e : 42},
       });
-      await getTileset(loader, '/hello/folder1/tileset_name.json');
-      expect(moxios.requests.mostRecent().url).toBe('ccc');
-    }
-    catch(e) {
-      expect(e).toBe(34);
-    }
+    });
+    // this double casting is necessary to create a fake loader without mocking the whole galaxy
+    // but still having TS accept it
+    await getTileset(loader as unknown as Phaser.Loader.LoaderPlugin, '/hello/folder1/tileset_name.json');
+    expect(moxios.requests.mostRecent().url).toBe('/hello/folder1/tileset_name.json');
+
   });
 });
